0935657f7d83840580ab9715f4608920c4030ff1
っ◔◡◔)っ ♥OD1NO4ka♥ <syperpyperpepper66@gmail.com>
2024-12-13 17:20:48 +0300

    123

--- Joski_Games/site/main.js
+++ Joski_Games/site/main.js
@@ -1,34 +1,10 @@
-// main.js
-new Vue({
-    el: '#app',
-    data: {
-        username: 'User Name',
-        games: [
-            { id: 'spy', name: 'Шпион', image: '/site/images/spy.png' },
-            { id: 'mafia', name: 'Мафия', image: '/site/images/mafia.jpg' },
-            { id: 'chess', name: 'Шахматы', image: '/site/images/chess.jpg' }
-        ],
-        messages: [],
-        newMessage: '',
-        baseUrl: 'http://localhost:8000'
-    },
-    
-    methods: {
-        startGame() {
-            const gameId = this.$event.currentTarget.getAttribute('data-game-id');
-            window.location.href = `/site/games/${gameId}.html`;
-        },
-        sendMessage() {
-            if (this.newMessage.trim()) {
-                const message = {
-                    id: Date.now(),
-                    username: this.username,
-                    text: this.newMessage.trim()
-                };
-                this.messages.push(message);
-                this.newMessage = '';
-                this.$refs.chatMessages.scrollTop = this.$refs.chatMessages.scrollHeight;
-            }
-        }
-    }
-});
+// Функции для запуска игры
+function startSoloGame() {
+    alert('Начало одиночной игры!');
+    // Логика запуска одиночной игры
+}
+
+function startMultiPlayerGame() {
+    alert('Создание лобби для многопользовательской игры!');
+    // Логика создания лобби
+}
