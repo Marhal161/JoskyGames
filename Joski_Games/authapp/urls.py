03177a56fc8fee73138e0ca2fcc39ab359a206a8
っ◔◡◔)っ ♥OD1NO4ka♥ <syperpyperpepper66@gmail.com>
2024-12-15 02:07:17 +0300

    123

--- Joski_Games/authapp/urls.py
+++ Joski_Games/authapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views.LoginView import LoginView
 from .views.LogoutView import LogoutView
 from .views.RegisterView import RegisterView
-from .views.pages import auth_page  # Импортируем auth_page
+from .views.pages import auth_page
 
 urlpatterns = [
-    path('login/', auth_page, name='login'),  # Рендер страницы auth.html для login
+    path('login/', auth_page, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', auth_page, name='register'),  # Рендер страницы auth.html для register
+    path('register/', auth_page, name='register'),
 ]
