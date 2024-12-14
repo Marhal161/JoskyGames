28980f463bd2ebf3136eb5b53728f80708b46c81
っ◔◡◔)っ ♥OD1NO4ka♥ <syperpyperpepper66@gmail.com>
2024-12-15 02:40:50 +0300

    123

---
+++ Joski_Games/Joski_Games/consumers.py
@@ -0,0 +1,18 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+
+        await self.send(text_data=json.dumps({
+            'username': self.scope['user'].username if self.scope['user'].is_authenticated else 'Anonymous',
+            'message': message
+        }))
